e371f9ba94986e26a730e9aa2338cb1e46dad067
ColtenMurray <60444923+ColtenMurray@users.noreply.github.com>
2020-04-22 17:54:30 -0500

    Add files via upload

---
+++ Pizza Shop-Order-Build-Decorate/src/patternsexamp/pizzaBuilder/PizzaOrder.java
@@ -0,0 +1,48 @@
+import java.awt.*;        // Using AWT container and component classes
+import java.awt.event.*;  // Using AWT event classes and listener interfaces
+
+// Using Command Design Patttern (code based on DZone Command Pattern)
+
+//Receiver
+public class PizzaOrder{
+  private boolean orderReady;
+  public Pizza.Builder pb;
+  public Pizza p;
+
+  public void bakeDeepDish(){
+	System.out.format("Baking a deepdish... \n"); 
+    pb = new Pizza.Builder("Deep Dish");
+    p.toppings.pepperoni=true;
+    p.toppings.frenchfries=true;
+    p.toppings.onion=true;
+    p = pb.pizzas(1).build();
+    reportOrder();
+
+  }
+  public void bakeThinCrust(){
+  	System.out.format("Baking a thincrust... \n");
+  	pb = new Pizza.Builder("Thin Crust");  
+    p = pb.pizzas(1).build();
+    p.toppings.pepperoni=true;
+    p.toppings.onion=true;
+    reportOrder();
+  }
+
+  public void bakeColtenPizza(){
+      System.out.format("Baking a Colten Pizza... \n");
+      pb = new Pizza.Builder("Colten Pizza");
+      p = pb.pizzas(1).build();
+      p.toppings.pepperoni=true;
+      p.toppings.frenchfries=true;
+      reportOrder();
+  }
+
+  private void reportOrder() {
+      System.out.format("\nBuilt %s - num pies: %d, pepperoni: %b, onion: %b, frenchfries %b\n", 
+    	p.getName(),  
+      p.getNumber(),
+      p.toppings.pepperoni,
+      p.toppings.onion,
+      p.toppings.frenchfries );
+  }
+}
---
+++ Pizza Shop-Order-Build-Decorate/src/patternsexamp/pizzaDecorator/PizzaExtras.java
@@ -0,0 +1,8 @@
+import java.awt.*;        // Using AWT container and component classes
+import java.awt.event.*;  // Using AWT event classes and listener interfaces
+
+// Using Decorator Design Patttern
+
+public interface PizzaExtras{
+    public Pizza addExtras();
+}
